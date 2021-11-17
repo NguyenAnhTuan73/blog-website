59e23778559e57760b5bd194e43278df07dc7f90
landing-page <nguyentuanqb73@gmail.com>
2021-11-17 23:10:04 +0700

    fix image

--- src/data.js
+++ src/data.js
@@ -1,16 +1,16 @@
 import styled, {createGlobalStyle} from "styled-components";
-import Img1 from '../src/assets/img1.jpg';
-import Img2 from '../src/assets/img2.jpg';
-import Img3 from '../src/assets/img3.jpg';
-import Img4 from '../src/assets/img4.jpg';
-import Img5 from '../src/assets/img5.jpg';
-import Img6 from '../src/assets/img6.jpg';
-import Img7 from '../src/assets/img7.jpg';
-import Img8 from '../src/assets/img8.jpg';
-import Img9 from '../src/assets/img9.jpg';
-import Img10 from '../src/assets/img10.jpg';
-import Img11 from '../src/assets/img11.jpg';
-import Img12 from '../src/assets/img12.jpg';
+import blogimg1 from '../src/assets/img1.jpg';
+import blogimg2 from '../src/assets/img2.jpg';
+import blogimg3 from '../src/assets/img3.jpg';
+import blogimg4 from '../src/assets/img4.jpg';
+import blogimg5 from '../src/assets/img5.jpg';
+import blogimg6 from '../src/assets/img6.jpg';
+import blogimg7 from '../src/assets/img7.jpg';
+import blogimg8 from '../src/assets/img8.jpg';
+import blogimg9 from '../src/assets/img9.jpg';
+import blogimg10 from '../src/assets/img10.jpg';
+import blogimg11 from '../src/assets/img11.jpg';
+import blogimg12 from '../src/assets/img12.jpg';
 
 export const Container = styled.div`
     width: 100%;
@@ -32,84 +32,84 @@ export const BlogData = [
         id: 1,
         title: 'Paris, France',
         description: 'What is HTML and why should you learn it?',
-        image: Img1,
+        image: blogimg1,
         date: 'November, 13, 2021'
     },
     {
         id: 2,
         title: 'Wadi Musa, Jordan',
         description: 'What is HTML and why should you learn it?',
-        image: Img2,
+        image: blogimg2,
         date: 'November, 13, 2021'
     },
     {
         id: 3,
         title: 'Khwaeng Wat Arun, Thailand',
         description: 'What is HTML and why should you learn it?',
-        image: Img3,
+        image: blogimg3,
         date: 'November, 13, 2021'
     },
     {
         id: 4,
         title: 'Abud, Indonesia',
         description: 'What is HTML and why should you learn it?',
-        image: Img4,
+        image: blogimg4,
         date: 'November, 13, 2021'
     },
     {
         id: 5,
         title: 'New York, United States',
         description: 'What is HTML and why should you learn it?',
-        image: Img5,
+        image: blogimg5,
         date: 'November, 13, 2021'
     },
     {
         id: 6,
         title: 'Marrakesh, Marocco',
         description: 'What is HTML and why should you learn it?',
-        image: Img6,
+        image: blogimg6,
         date: 'November, 13, 2021'
     },
     {
         id: 7,
         title: 'Banaue, Philippines',
         description: 'What is HTML and why should you learn it?',
-        image: Img7,
+        image: blogimg7,
         date: 'November, 13, 2021'
     },
     {
         id: 8,
         title: 'Macows, Rusia',
         description: 'What is HTML and why should you learn it?',
-        image: Img8,
+        image: blogimg8,
         date: 'November, 13, 2021'
     },
     {
         id: 9,
         title: 'Alaska, Canada',
         description: 'What is HTML and why should you learn it?',
-        image: Img9,
+        image: blogimg9,
         date: 'November, 13, 2021'
     },
     {
         id: 10,
         title: 'HalongBay, VietNam',
         description: 'What is HTML and why should you learn it?',
-        image: Img10,
+        image: blogimg10,
         date: 'November, 13, 2021'
     },
     {
         id: 11,
         title: 'Schwangau, Deutschland',
         description: 'What is HTML and why should you learn it?',
-        image: Img11,
+        image: blogimg11,
         date: 'November, 13, 2021'
     },
     {
         id: 12,
         title: 'Roma, Italy',
         description: 'What is HTML and why should you learn it?',
-        image: Img12,
+        image: blogimg12,
         date: 'November, 13, 2021'
     },
 
